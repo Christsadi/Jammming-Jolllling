ea377625467d68cb41779a4e70071762557c197a
Christ Sadi <christsadi@gmail.com>
2022-04-19 09:32:13 +0200

    create an empty JavaScript module called Spotify

---
+++ src/util/Spotify.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Spotify extends React.Component {
+    constructor (props) {
+      super(props);
+         this.getAccessToken = this.getAccessToken.bind(this);
+         
+    }
+
+    getAccessToken(){
+
+    }
+
+    render(){
+      return (
+        
+      )
+    }
+  }
+
+
+
+export default Spotify;
