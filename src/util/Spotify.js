762c31d7c4b5899f1562b8c51c096978f69c22e2
Christ Sadi <christsadi@gmail.com>
2022-04-20 12:15:57 +0200

    Obtain a Spotify Access Token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,20 +1,40 @@
 import React from 'react';
+const clientId = '6f0f305fca8b4660b99c8184aead6cb5';
+const redirectUri ='http://localhost:3000/';
 
-class Spotify extends React.Component {
-    constructor (props) {
-      super(props);
-         this.getAccessToken = this.getAccessToken.bind(this);
-         
-    }
+let accessToken;
 
+const Spotify = {
     getAccessToken(){
+        if(accessToken){
+            return accessToken;
+        } 
+            // check for acces token match
+            const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+            const expirationTimeMatch = window.location.href.match(/expires_in=([^&]*)/)
 
-    }
+            if(accessTokenMatch && expirationTimeMatch){
+                accessToken = accessTokenMatch[1];
+                const expiresIn = Number(expirationTimeMatch[1]);
+                
+                //this code, wipes the access token and URL parameters.
+                window.setTimeout(() => accessToken = '', expiresIn * 1000);
+                window.history.pushState('Access Token', null, '/');
+                return accessToken;
 
-    render(){
-      return (
-        
-      )
+            } else {
+                const accessUrl = `https://accounts.spotify.com/authorize?
+                           client_id=${clientId}&response_type=token&scope=playlist-modify-public&
+                           redirect_uri=${redirectUri}`;
+                
+                windows.location = accessUrl;
+            }
+
+            
+
+
+
+
     }
   }
 
