fdfacc5a94453c61839d7064846764b2a105a318
Christ Sadi <christsadi@gmail.com>
2022-06-29 09:53:40 +0200

    added event listener for enter button

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    
     this.state = {
-      term: ''
+      term: ""
     };
-
-       this.search = this.search.bind(this);
-       this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.enterButton = this.enterButton.bind(this);
   }
 
   search(){
@@ -21,11 +20,17 @@ class SearchBar extends React.Component {
     this.setState({term: event.target.value})
   }
 
+  enterButton(event) {
+    if (event.key === "Enter") {
+        this.search();
+    }
+}
+
   render(){
     return (
         <div className='SearchBar'>
             <input placeholder="Enter A Song, Album, or Artist" 
-                  onChange={this.handleTermChange}/>
+                  onChange={this.handleTermChange} onKeyPress={this.enterButton}/>
             <button onClick = {this.search} className='SearchButton'>SEARCH</button>
         </div>
     )
